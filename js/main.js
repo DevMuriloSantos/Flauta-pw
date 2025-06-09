be414f7c5e3f045ff998e8311d979cbb83ea8b1f
Murilo <murilopaulo051@gmail.com>
2025-06-09 19:44:10 -0300

    atualização inicial

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+/*document.addEventListener('contextmenu', event => event.preventDefault());
+
+    document.addEventListener('keydown', function (e) {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
+    (e.ctrlKey && e.key === 'U')
+  ) {
+    e.preventDefault();
+  }
+});*/
+
