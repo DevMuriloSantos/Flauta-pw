b50eb50a659c7df67aa2302074b2849408ff54af
Murilo <murilopaulo051@gmail.com>
2025-06-13 21:24:48 -0300

    atualização 1.1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
+const pergunta = document.getElementById('music');
 
+pergunta.addEventListener('click', function() {
+    const resposta = prompt('Digite a senha para continuar:');
+    if (resposta === 'flauta') {
+        window.location.href = 'https://github.com/DevMuriloSantos/Flauta-pw';
+    } else {
+        alert('Senha incorreta. Tente novamente.');
+    }
+});
 
-/*document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
     document.addEventListener('keydown', function (e) {
   if (
-    e.key === 'F12' ||
-    (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
-    (e.ctrlKey && e.key === 'U')
+    e.key === 'F11' ||
+    (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase()))
   ) {
     e.preventDefault();
   }
-});*/
+});
 
